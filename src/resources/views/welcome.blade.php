818801b1c460c04eaf5295ef98c062fcdca7284f
Szurag <bartosz.lemieszko@gmail.com>
2023-09-05 08:22:32 +0200

    react init

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,1 +1,16 @@
-<h1>TrayDROP!!!</h1>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Traydrop!</title>
+
+        @viteReactRefresh
+        @vite('resources/js/index.jsx')
+    </head>
+    <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="root"></div>
+    </body>
+</html>
