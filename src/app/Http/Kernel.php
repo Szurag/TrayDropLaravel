e16044bf9cdc643d64352abad0799c6c1e74d327
Szurag <bartosz.lemieszko@gmail.com>
2023-09-08 11:10:34 +0200

    thorttling

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
 //            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-//            'throttle:2137,1',
+            'throttle:60,1',
             \App\Http\Middleware\Cors::class,
         ],
     ];
